25608eb868e36e9a555e6942ca24eba34fad12ff
Shaahid <shaahid2453@gmail.com>
2025-01-14 09:05:01 -0800

    changes due to eslint

--- src/components/Forms/Fetchyt.tsx
+++ src/components/Forms/Fetchyt.tsx
@@ -1,30 +1,30 @@
-// import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import { OAuth2Client } from 'google-auth-library';
+// // import { useEffect } from 'react';
+// import { useRouter } from 'next/router';
+// import { OAuth2Client } from 'google-auth-library';
 
-export default function Home() {
-  const router = useRouter();
+// export default function Home() {
+//   const router = useRouter();
 
-  const startOAuthFlow = async () => {
-    const oauth2Client = new OAuth2Client(
-      process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
-      process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
-      process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI
-    );
+//   const startOAuthFlow = async () => {
+//     const oauth2Client = new OAuth2Client(
+//       process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+//       process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
+//       process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI
+//     );
 
-    // Generate the authorization URL
-    const authUrl = oauth2Client.generateAuthUrl({
-      access_type: 'offline', // Request a refresh token
-      scope: ['https://www.googleapis.com/auth/youtube.force-ssl'], 
-    });
+//     // Generate the authorization URL
+//     const authUrl = oauth2Client.generateAuthUrl({
+//       access_type: 'offline', // Request a refresh token
+//       scope: ['https://www.googleapis.com/auth/youtube.force-ssl'], 
+//     });
 
-    router.push(authUrl);
-  };
+//     router.push(authUrl);
+//   };
 
-  return (
-    <div>
-      <h1>YouTube Transcript Fetcher</h1>
-      <button onClick={startOAuthFlow}>Sign in with Google</button>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <h1>YouTube Transcript Fetcher</h1>
+//       <button onClick={startOAuthFlow}>Sign in with Google</button>
+//     </div>
+//   );
+// }
